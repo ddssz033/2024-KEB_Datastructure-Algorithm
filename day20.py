28c1c5f6f996c4d175ce9ce79029807749a199a6
ddssz033 <ddssz033@gmail.com>
2024-02-08 13:56:51 +0900

    v0.1

---
+++ day20.py
@@ -0,0 +1,18 @@
+import random
+
+answer = random.randint(1, 100)
+chance = 7
+
+while chance != 0:
+    guess = int(input("Input guess number : "))
+    if guess == answer:
+        print(f'You win. Answer is {answer}')
+        break
+    elif guess > answer:
+        chance = chance - 1
+        print(f'{guess} is bigger. Chance left : {chance}')
+    else:
+        chance = chance - 1
+        print(f'{guess} is lower. Chance left : {chance}')
+else:
+    print(f'You lost. Answer is {answer}')
