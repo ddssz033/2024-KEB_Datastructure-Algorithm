8ad1ac3511b428a2c2e96a340228b8fbc22fe6a2
ddssz033 <ddssz033@gmail.com>
2024-02-14 16:13:01 +0900

    v0.3

--- day22.py
+++ day22.py
@@ -1,9 +1,8 @@
 def decimal_to_octal(number: int) -> str:
-    octal = ''
-    while number > 0:
-        octal = str(number % 8) + octal
-        number = number // 8
-    return octal
+    if number < 8:
+        return str(number)
+    else:
+        return decimal_to_octal(n//8) + str(n % 8)
 
 n = int(input("input decimal number : "))
 print(decimal_to_octal(n))
