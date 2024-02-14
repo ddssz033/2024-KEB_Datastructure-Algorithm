4eddee18a2d8fa14845048f55fa92d36d66051df
ddssz033 <ddssz033@gmail.com>
2024-02-14 15:25:48 +0900

    v0.1

--- day21.py
+++ day21.py
@@ -1,20 +1,3 @@
-import random
-
-dataArray = []
-for _ in range(7):
-    dataArray.append(random.randint(1, 100))
-
-node = Node()
-node.data = dataArray[0]
-head = node
-node.link = head
-memory.append(node)
-
-for data in dataArray[1:]:
-    pre = node
-    node = Node()
-    node.data = data
-    pre.link = node
-    node.link = head
-    memory.append(node)
+import webbrowser
+webbrowser.open("www.instagram.com")
 
---
+++ day22.py
@@ -0,0 +1,18 @@
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
+
+graph = [
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0]
+]
+
+visited = [0 for _ in range(len(graph))]
+dfs(graph, 0, visited)
