fba3905b3b59da79011292b3066661f9484c8a24
ddssz033 <ddssz033@gmail.com>
2024-02-16 11:38:09 +0900

    v0.3

--- day24.py
+++ day24.py
@@ -1,26 +1,37 @@
-# 동적 프로그래밍을 사용한 피보나치 수열 계산
-def fibonacci_dynamic(n):
-    fib = [0] * (n + 1)
-    fib[1] = 1
-    for i in range(2, n + 1):
-        fib[i] = fib[i - 1] + fib[i - 2]
-    return fib[n], n + 1
+from collections import deque  # for bfs
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
-# 재귀를 사용한 피보나치 수열 계산
-def fibonacci_recursive(n):
-    if n <= 1:
-        return n, 1
-    else:
-        fib_1, count_1 = fibonacci_recursive(n - 1)
-        fib_2, count_2 = fibonacci_recursive(n - 2)
-        return fib_1 + fib_2, count_1 + count_2 + 1
+def bfs(g, i, visited):
+    queue = deque([i])
+    visited[i] = 1
+    while queue:
+        #print(visited)
+        i = queue.popleft()
+        print(chr(ord('A') + i), end=' ')
+        for j in range(len(g)):
+            if g[i][j] == 1 and not visited[j]:
+                queue.append(j)
+                visited[j] = 1
 
-# 각 방법의 결과 및 연산 횟수 비교
-n = 30
-fib_dynamic, count_dynamic = fibonacci_dynamic(n)
-fib_recursive, count_recursive = fibonacci_recursive(n)
 
-print("동적 프로그래밍 결과:", fib_dynamic)
-print("동적 프로그래밍 연산 횟수:", count_dynamic)
-print("재귀 결과:", fib_recursive)
-print("재귀 연산 횟수:", count_recursive)
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
+
+visited_dfs = [0 for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph, 0, visited_dfs)
+print()
+bfs(graph, 0, visited_bfs)
